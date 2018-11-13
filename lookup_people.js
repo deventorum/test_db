9740fa371f7d2b42cda96a0d2fbcd5c74a2fa37a
deventorum <deventorum@gmail.com>
2018-11-13 17:36:46 +0000

    Code refactored for Knex

--- lookup_people.js
+++ lookup_people.js
@@ -1,18 +1,30 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
 const dataProcess = require("./functions")
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  }
+});
+const myArgs = process.argv.slice(2)[0]
 
-const client = new pg.Client({
+/* const client = new pg.Client({
   user     : settings.user,
   password : settings.password,
   database : settings.database,
   host     : settings.hostname,
   port     : settings.port,
   ssl      : settings.ssl
-});
-const myArgs = process.argv.slice(2)[0]
+}); */
 
-client.connect((err) => {
+
+/* client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
@@ -24,4 +36,14 @@ client.connect((err) => {
     dataProcess.printResults(result.rows, myArgs)
     client.end();
   });
-});
+}); */
+
+knex.select('*').from('famous_people')
+  .where({first_name: myArgs})
+  .then(rows => {
+    console.log('Searching ...');
+    dataProcess.printResults(rows, myArgs)
+  })
+  .finally(function() {
+    knex.destroy();
+  })
